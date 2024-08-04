73ab9080af0bd2353e4139b7289f0b6b8fcc0480
Tamabcxyz <minhtam16071999@gmail.com>
2024-08-04 08:04:36 -0400

    basic knowledge of Makefile :tada:

---
+++ ProjectTemplate/src/my_lib.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "my_lib.h"
+void print_hello_world() {
+    printf("Hello wourld!\n");
+}
---
+++ WildcarsFiles/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "my_lib.h"
+int main(int argc, char *argv[])
+{
+    print_hello_world();
+    return 0;
+}
